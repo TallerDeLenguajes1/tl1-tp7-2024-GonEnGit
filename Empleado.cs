005214d53075157da076246f2b99f74506030620
Gonzalo Martinez <mgonzalo192@gmail.com>
2024-06-17 12:32:11 -0300

    finalmente terminado el TP07

--- Empleado.cs
+++ Empleado.cs
@@ -1,28 +1,73 @@
+
 namespace EspacioEmpleado;
 
 public partial class Empleado
 {
     private string nombre;
     private string apellido;
-    private DateTime nacimiento; // revisa los date time
+    private DateOnly nacimiento;
     private char estadoCivil;
-    private DateTime ingresoAEmpresa;
-    private double sueldoBasico;
+    private DateOnly ingresoAEmpresa;
+    private double sueldoBasico = 150000;
+    private double salario;
     private cargo cargoDelEmpleado;
 
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apellido { get => apellido; set => apellido = value; }
-    public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+    public DateOnly Nacimiento { get => nacimiento; set => nacimiento = value; }
     public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
-    public DateTime IngresoAEmpresa { get => ingresoAEmpresa; set => ingresoAEmpresa = value; }
+    public DateOnly IngresoAEmpresa { get => ingresoAEmpresa; set => ingresoAEmpresa = value; }
     public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
     public cargo CargoDelEmpleado { get => cargoDelEmpleado; set => cargoDelEmpleado = value; }
+    public double Salario { get => salario; set => salario = value; }
 
     public enum cargo { 
         Auxiliar, 
         Administrativo, 
         Ingeniero, 
-        Especialista, 
+        Especialista,
         Investigador, }
+
+    public int calcularAntiguedad(DateOnly ingre)
+    {
+        int antiguedad;
+        DateTime hoy = DateTime.Now;
+
+        antiguedad = hoy.Year - ingre.Year;
+
+        return antiguedad;
+    }
+
+    public double calcularSalario()
+    {
+        int antiguedad = calcularAntiguedad(ingresoAEmpresa);
+        double adicional;
+        double total;
+
+        if (antiguedad >= 20)
+        {
+            adicional = 0.25;
+        }
+        else
+        {
+            adicional = antiguedad * 0.1;
+        }
+
+        if (CargoDelEmpleado == (cargo)2 || cargoDelEmpleado == (cargo)3)
+        {
+            adicional = adicional + 0.5;
+        }
+
+        if (estadoCivil == 'C')
+        {
+            total = sueldoBasico + (sueldoBasico * adicional) + 150000;
+        }
+        else
+        {
+            total = sueldoBasico + (sueldoBasico * adicional);
+        }
+
+        return total;
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,45 @@
 ﻿
 using EspacioEmpleado;
 
+// tené cuidado, esto no está creando un arreglo con 3 objetos vacios
+// esta linea solo crea un arreglo con 3 referencias a Empleado
+// despues igual tenes que crear los 3 objetos
 Empleado[] arreglo = new Empleado[3];
-DateTime[] nacimientos = {new DateTime(1986, 2, 15), new DateTime(1990, 4, 21), new DateTime(1997, 9, 5) };
-DateTime[] ingresos = {new DateTime(2015, 4, 20), new DateTime(2012, 5, 8), new DateTime(2020, 7, 21) };
+
+// DateOnly es la version de solo fecha de DateTime
+// tambien existe 'TimeOnly' que es la version de solo
+// hora de DateTime, todas son la misma estructura de DateTime
+DateOnly[] nacimientos = {new DateOnly(1986, 2, 15), new DateOnly(1990, 4, 21), new DateOnly(1997, 9, 5) };
+DateOnly[] ingresos = {new DateOnly(1998, 4, 20), new DateOnly(2010, 5, 8), new DateOnly(2020, 7, 21) };
+
+/* variables */
 char estado;
 bool prueba;
 string ingreso;
 int valorCargo;
 double valorSueldo;
+double totalAPagar = 0;
 
+
+/* cuerpo del programa */
 for (int i = 0; i < 3; i++)
 {
+// aquí primero creas el objeto EMpleado y recien guardas los datos
+// NOTA: si pones Empleado arreglo [i] = ... te dice que estas
+// redefiniendo arreglo[i]
+    arreglo[i] = new Empleado();
     valorSueldo = new Random().NextDouble() * 1000 + 5000;
     valorCargo = new Random().Next(1, 5);
 
-    Console.WriteLine($"Ingrese el nombre del empleado N°{i}: ");
+    Console.WriteLine($"\nIngrese el nombre del empleado N°{i+1}: ");
     arreglo[i].Nombre = Console.ReadLine();
 
-    Console.WriteLine($"Ingrese el apellido del empleado N°{i}: ");
+    Console.WriteLine($"Ingrese el apellido del empleado N°{i+1}: ");
     arreglo[i].Apellido = Console.ReadLine();
 
     arreglo[i].Nacimiento = nacimientos[i];
 
-    Console.WriteLine($"Ingrese el estado civil del empleado N°{i} (S/C): ");
+    Console.WriteLine($"Ingrese el estado civil del empleado N°{i+1} (S/C): ");
     ingreso = Console.ReadLine();
     prueba = char.TryParse(ingreso, out estado);
     if (prueba)
@@ -37,12 +53,33 @@ for (int i = 0; i < 3; i++)
 
     arreglo[i].IngresoAEmpresa = ingresos[i];
 
+// usar el enum era mas facil de lo que parecia
+
+// '(nombre del enum)numero' te devuelve el valor de enum
+// guardado en la posicion 'numero'
+
+// '(int)valor del enum' te devuelve el numero
+// que corresponde al valor
     arreglo[i].CargoDelEmpleado = (Empleado.cargo)valorCargo;
+
+    arreglo[i].Salario = arreglo[i].calcularSalario();
 }
 
 foreach (var item in arreglo)
 {
-    Console.WriteLine($"{item.Nombre}, {item.Apellido}, {item.CargoDelEmpleado}");
-}
+    Console.WriteLine($"\nNombre: {item.Nombre}, {item.Apellido}");
+    Console.WriteLine($"Nacido: {item.Nacimiento}, Ingresó: {item.IngresoAEmpresa}");
+    if (item.EstadoCivil == 'S')
+    {
+        Console.WriteLine($"Estado civil: Soltero");
+    }
+    else
+    {
+        Console.WriteLine($"Estado civil: Casado");
+    }
+    Console.WriteLine($"Cargo: {item.CargoDelEmpleado}, Antiguedad: {item.calcularAntiguedad(item.IngresoAEmpresa)}");
+    Console.WriteLine("Sueldo: $ " + item.calcularSalario());
 
-/* te está dando un error */ 
+    totalAPagar = totalAPagar + item.calcularSalario();
+}
+Console.WriteLine($"\nTotal en sueldos: $ {totalAPagar}\n");
